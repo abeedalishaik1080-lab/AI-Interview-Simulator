d4a23310b03e4ecf0cbd7a0670f5db18e780529f
abeedalishaik1080-lab <abeedalishaik1080@gmail.com>
2026-07-04 10:52:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 # Added dot (.) markers for absolute relative parsing inside Vercel
-from .routers.upload import router as upload_router
-from .routers.interview import router as interview_router
-
-from .database.database import Base, engine
-from .models.interview import Interview
-
+from routers.upload import router as upload_router
+from routers.interview import router as interview_router
+from database.database import Base, engine
+from models.interview import Interview
 app = FastAPI(title="AI Interview Simulator API")
 
 Base.metadata.create_all(bind=engine)
